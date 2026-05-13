58e8cd53823d3fe234477dedb4bf5f8bfbc5e3e6
ailinconstruction789 <ailinconstruction789@gmail.com>
2026-05-13 13:37:20 +0700

    fix config warning

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // !! สั่งให้ Vercel ข้ามการเช็ก TypeScript Error ไปเลย !!
+    // สั่งให้ Vercel ข้ามการเช็ก TypeScript Error
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // !! สั่งให้ Vercel ข้ามการเช็ก ESLint ด้วย !!
-    ignoreDuringBuilds: true,
   },
 };
 
